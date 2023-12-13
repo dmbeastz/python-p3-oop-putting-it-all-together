3f225cfe2fa152c1f9db837d978ce412f94b26e0
dmbeastz <daveroymutisya2@gmail.com>
2023-12-13 11:45:04 +0300

    Completed Lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title = "And Then They were none", page_count = 272):
+        self.title = title
+        self.__title = None
+        self.__page_count = None
+        self.page_count = page_count
+    def get_page_count (self):
+        return self.__page_count 
+    def get_title (self):
+        return self.__title
+    def set_count (self,count):
+        if type (count) == int:
+            self.__page_count = count
+        else:
+            print("page_count must be an integer")
+    def turn_page (self):
+        print ("Flipping the page...wow, you read fast!")
+    page_count = property(get_page_count,set_count)
+
+    
+    
+        
+  
     
         
